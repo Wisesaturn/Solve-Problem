98d0cbe484cc560c6245c6f7e8af311c7b70316b
Jae Han Song <rfv1479@gmail.com>
2022-01-03 15:47:47 +0900

    로또의 최고 순위와 최저 순위

---
+++ 프로그래머스/Level 1/01_로또의 최고 순위와 최저 순위.cpp
@@ -0,0 +1,35 @@
+// Since 2022-01-03
+// 문제 설명 : https://programmers.co.kr/learn/courses/30/lessons/77484
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> lottos, vector<int> win_nums)
+{
+    vector<int> answer;
+    vector<int> result(2);
+    vector<int>::iterator it;
+
+    for (size_t i = 0; i < 2; i++) {
+        for (size_t j = 0; j < 6; j++) {
+            if (i == 0) { // 최고일 때
+                it = find(win_nums.begin(), win_nums.end(), lottos[j]);
+                if (it != win_nums.end() || lottos[j] == 0)
+                    result[i]++;    
+            } else { // 최저일 때
+                it = find(win_nums.begin(), win_nums.end(), lottos[j]);
+                if(it != win_nums.end())
+                    result[i]++;
+            }
+        }
+        if (result[i] != 0)
+            result[i] = (result[i] - 7) * -1;
+        else {
+            result[i] = 6;
+        }
+
+    return result;
+}
